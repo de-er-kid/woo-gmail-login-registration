f5d687a5f2a1d6a4aa88caf24396694e956c430c
de-er-kid <87042021+de-er-kid@users.noreply.github.com>
2023-06-11 13:28:29 +0530

    Update woo-gmail-login-registration.php

--- woo-gmail-login-registration.php
+++ woo-gmail-login-registration.php
@@ -10,6 +10,7 @@ License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 Text Domain: woocommerce-gmail-login
 Domain Path: /languages
+GitHub Plugin URI: de-er-kid/woo-off-sale-percentage
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
